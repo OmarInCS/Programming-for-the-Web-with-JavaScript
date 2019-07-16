c5582747a73e946f0eaf735645ff1a93e695075a
Omar Karem <Omarincs@gmail.com>
2019-07-16 16:34:32 +0300

    Checkbox functionality - trial #1

--- Homework_4/FontChooser.js
+++ Homework_4/FontChooser.js
@@ -3,26 +3,32 @@ class FontChooser extends React.Component {
 
     constructor(props) {
 	super(props);
-	this.state = {hidden: true, bold: this.props.bold, size: this.props.size}
+	this.state = {hidden: true, isBold: this.props.bold, size: this.props.size};
     }
 
     toggleFontChooser() {
-    	this.setState({hidden : !this.state.hidden})
+    	this.setState({hidden : !this.state.hidden});
+    }
+
+    toggleBold() {
+    	this.setState({isBold : !this.state.isBold});
     }
     
 
     render() {
 
-		var bold = this.state.bold? 'bold' : 'normal';
+		var bold = this.state.isBold? 'bold' : 'normal';
 
 		return(
 		       <div>
-		       <input type="checkbox" id="boldCheckbox" hidden={this.state.hidden}/>
+		       <input type="checkbox" id="boldCheckbox" 
+		       		hidden={this.state.hidden} {this.state.isBold? "checked" : ""
+		       		onChange={this.toggleBold.bind(this)}>
 		       <button id="decreaseButton" hidden={this.state.hidden}>-</button>
 		       <span id="fontSizeSpan" hidden={this.state.hidden}>{this.props.size}</span>
 		       <button id="increaseButton" hidden={this.state.hidden}>+</button>
 		       <span id="textSpan" onClick={this.toggleFontChooser.bind(this)} 
-		       	style={{fontWeight: bold, fontSize: this.state.size}}>
+		       	style={{fontWeight: bold, fontSize: this.state.size +"px"}}>
 		       		{this.props.text}
 		       </span>
 		       </div>
